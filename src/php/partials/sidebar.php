60631653a0c73cbd079f7ba779ce7282e23ea9d4
Geert Heemskerk <leaguecrafterhd@gmail.com>
2016-08-29 16:32:59 +0200

    Made the user template so we can use that template to make the admin page

---
+++ src/php/partials/sidebar.php
@@ -0,0 +1,29 @@
+        <div class="col col_quarter sidebar" >
+            <img class="competa_logo" src="../../img/home/logo.png"/>
+            <p class="welcome_message">Welcome, name</p>
+            <nav class="navigation-container">
+                <ul class="navigation-list">
+                    <li class="navigation-item">
+                        Interns
+                        <a class="navigation-item-link"></a>
+                    </li>
+                    <li class="navigation-item">
+                        Employees
+                        <a class="navigation-item-link"></a>
+                    </li>
+                    <li class="navigation-item">
+                        Trainees
+                        <a class="navigation-item-link"></a>
+                    </li>
+                    <li class="navigation-item">
+                        Back-office
+                        <a class="navigation-item-link"></a>
+                    </li>
+                </ul>
+            </nav>
+            <button class="logout_button">
+                <div class="logout-img">
+                </div>
+            </button>
+        </div>
+
---
+++ src/php/partials/user_content.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../../../build/css/style.min.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <?php include_once 'sidebar.php';?>
+        <div class="col content">
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">shabalaba ding dong</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+        </div>
+    </body>
+</html>
