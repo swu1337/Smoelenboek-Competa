af1badf1eddfab249fff00d73b7d8b2f090546a9
NandoIT <nando@competa.com>
2016-08-30 10:06:32 +0200

    Scroll fix

--- src/php/partials/user_content.php
+++ src/php/partials/user_content.php
@@ -32,8 +32,50 @@
                 <img class ="add-person" src="../../../src/img/home/add-person.png"/>
                 <figcaption class="photo-description">container</figcaption>
             </figure>
-       
-
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">container</figcaption>
+            </figure>
         </div>
     </body>
 </html>
