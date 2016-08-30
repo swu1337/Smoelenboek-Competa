19026a71450588ba3a0df2e8e5076addf98226d6
Geert Heemskerk <leaguecrafterhd@gmail.com>
2016-08-30 11:21:58 +0200

    Made the styling for delete popup so the admin can delete persons

---
+++ src/php/partials/admin_delete.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../../../build/css/style.min.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <?php include_once 'sidebar.php';?>
+        <div class="col content">
+
+            <!--Add a if statement to activate the div-->
+            <div class="delete_popup">
+                <p>Are you sure that you want to delete (name)?</p>
+                <button class="confirm_delete">Yes</button>
+                <button class="decline_delete">No</button>
+            </div>
+
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+            <figure class="col photo-folder">
+                <img class ="add-person" src="../../../src/img/home/add-person.png"/>
+                <figcaption class="photo-description">Geert Heemskerk</figcaption>
+            </figure>
+        </div>
+    </body>
+</html>
